9e4d1ef3c52a5e3f0e096ba1173ee7eca5185785
Ahsan Sajjad <princeahsan0608@gmail.com>
2024-01-03 12:13:29 +0500

    ..

--- Components/src/App.jsx
+++ Components/src/App.jsx
@@ -1,18 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-// import Home from './Components/Home/Home';
-// import Login from './Components/Login/Login';
-import States from './Components/UseState/state';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        
-{/* <Home/> */}
 
-<States/>
-
+     
       </header>
     </div>
   );
