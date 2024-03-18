e3097d0d74db88351616e520e6f4a51623d0a9b1
Ahsan Sajjad <princeahsan0608@gmail.com>
2024-03-18 12:47:55 +0500

    Basic Implementation of Context Api

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,12 +1,19 @@
+import Profile from "./components/Profile"
+import UserContextProvider from "./context/UserContextProvider"
+import Login from "./components/Login"
+
 
 
 function App() {
- 
+
 
   return (
-    <>
-     
-    </>
+    <UserContextProvider>
+
+      <Login />
+      <Profile />
+
+    </UserContextProvider>
   )
 }
 
